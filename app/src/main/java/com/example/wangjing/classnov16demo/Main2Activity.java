316743615a232b6a78ba7371dce681c2e3d2e471
crystalwang0124 <crystalwang0124@gmail.com>
2016-11-17 01:25:17 -0500

    For Nov 16 Class Demo

---
+++ app/src/main/java/com/example/wangjing/classnov16demo/Main2Activity.java
@@ -0,0 +1,51 @@
+package com.example.wangjing.classnov16demo;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Main2Activity extends Activity implements View.OnClickListener {
+
+    //declare objects here
+    private Button buttonGo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        //Link to UI
+        buttonGo = (Button) findViewById(R.id.buttonGo);
+        //start a listener
+        buttonGo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //start a DialogBox
+        new AlertDialog.Builder(this)
+                .setMessage("Where Do You Want to Go?")
+                .setPositiveButton("To Start Page",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                        })
+                .setNegativeButton("Stay Here",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(Main2Activity.this, "We will stay here", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+    }
+}
